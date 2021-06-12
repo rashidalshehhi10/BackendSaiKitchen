fd9f57e97143b62fe4594af80385b17a9383b5a6
Ali Burhan <ali_burhan1993@outlook.com>
2021-06-12 11:24:59 +0300

    DesignAddFiles

---
+++ BackendSaiKitchen/Controllers/DesignController.cs
@@ -0,0 +1,57 @@
+﻿using BackendSaiKitchen.CustomModel;
+using BackendSaiKitchen.Helper;
+using BackendSaiKitchen.Models;
+using Microsoft.AspNetCore.Mvc;
+using SaiKitchenBackend.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendSaiKitchen.Controllers
+{
+    public class DesignController : BaseController
+    {
+        static List<File> files = new List<File>();
+        [HttpPost]
+        [Route("[action]")]
+        public async Task<object> AddUpdateDesignfiles(DesignCustomModel designCustomModel)
+        {
+            files.Clear();
+            var inquiryworkscope = inquiryWorkscopeRepository.FindByCondition(x => x. == de.Ininquiryworkscopeid && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+            foreach (var file in designCustomModel.base64f3d)
+            {
+                string fileUrl = await Helper.Helper.UploadFileToBlob(file);
+
+                if (fileUrl != null)
+                {
+                    files.Add(new File()
+                    {
+                        FileUrl = fileUrl,
+                        FileName = fileUrl.Split('.')[0],
+                        IsActive = true,
+                        IsDeleted = false,
+                        UpdatedBy = Constants.userId,
+                        UpdatedDate = Helper.Helper.GetDateTime(),
+                        CreatedBy = Constants.userId,
+                        CreatedDate = Helper.Helper.GetDateTime(),
+
+                    });
+
+                }
+                else
+                {
+                    response.isError = true;
+                    response.errorMessage = Constants.wrongFileUpload;
+                }
+            }
+
+            Measurement measurement = new Measurement() { MeasurementTakenBy = Constants.userId, Files = files };
+            inquiryworkscope.InquiryStatusId = (int)inquiryStatus.measurementWaitingForApproval;
+            inquiryworkscope.Measurements.Add(measurement);
+            inquiryWorkscopeRepository.Update(inquiryworkscope);
+            context.SaveChanges();
+            return response;
+        }
+    }
+}
---
+++ BackendSaiKitchen/CustomModel/DesignCustomModel.cs
@@ -0,0 +1,16 @@
+﻿using BackendSaiKitchen.Models;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendSaiKitchen.CustomModel
+{
+    public class DesignCustomModel
+    {
+        public int inquiryWorkScopeId { get; set; }
+
+        public List<byte[]> base64f3d { get; set; }
+    }
+}
