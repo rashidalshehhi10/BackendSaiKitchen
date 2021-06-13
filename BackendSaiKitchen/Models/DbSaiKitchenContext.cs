37c23bdfe12fa0dc0d535f1c7cccce4be13f3006
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-06-13 09:38:50 +0400

    Db Updated

--- BackendSaiKitchen/Models/DbSaiKitchenContext.cs
+++ BackendSaiKitchen/Models/DbSaiKitchenContext.cs
@@ -55,7 +55,7 @@ namespace BackendSaiKitchen.Models
             if (!optionsBuilder.IsConfigured)
             {
 #warning To protect potentially sensitive information in your connection string, you should move it out of source code. You can avoid scaffolding the connection string by using the Name= syntax to read it from configuration - see https://go.microsoft.com/fwlink/?linkid=2131148. For more guidance on storing connection strings, see http://go.microsoft.com/fwlink/?LinkId=723263.
-                optionsBuilder.UseSqlServer("Server=.\\;Database=DbSaiKitchen;Trusted_Connection=True;MultipleActiveResultSets=true;");
+                optionsBuilder.UseSqlServer("Server=backendsaikitchendbserver.database.windows.net,1433;Database=BackendSaiKitchen_db;User Id=SaiAdmin;Password=SaiKitchen123;MultipleActiveResultSets=true;");
             }
         }
 
