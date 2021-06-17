08479d87338fa86fade3448057f2571af104f6f5
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-06-17 09:24:56 +0400

    Notification Date Added

--- BackendSaiKitchen/CustomModel/NotificationResponse.cs
+++ BackendSaiKitchen/CustomModel/NotificationResponse.cs
@@ -6,6 +6,7 @@
         public string notificationContent { get; set; }
         public int? notificationCategoryId { get; set; }
         public string notificationCategoryName { get; set; }
+        public string createdDate { get; set; }
         public bool? isActionable { get; set; }
         public object notificationAcceptAction { get; set; }
         public object notificationDeclineAction { get; set; }
