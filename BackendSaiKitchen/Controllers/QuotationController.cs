3f173216dc163653f57c08ed793df34f9b85e8ff
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-06-29 11:53:40 +0400

    Workscope added on Quotation

--- BackendSaiKitchen/Controllers/QuotationController.cs
+++ BackendSaiKitchen/Controllers/QuotationController.cs
@@ -23,7 +23,7 @@ namespace BackendSaiKitchen.Controllers
         {
            var inquiry = inquiryRepository.FindByCondition(x => x.InquiryId == inquiryId && x.InquiryWorkscopes.Any(y => y.IsActive == true && y.IsDeleted == false) && x.IsActive == true
                 && x.IsDeleted == false && (x.InquiryWorkscopes.Where(y => y.IsActive == true && y.IsDeleted == false).Count() == x.InquiryWorkscopes.Where(y => y.InquiryStatusId == (int)inquiryStatus.quotationPending && y.IsActive == true && y.IsDeleted == false).Count()))
-               .Include(x=>x.Customer).Include(x=>x.Building)
+               .Include(x=>x.Customer).Include(x=>x.Building).Include(x=>x.InquiryWorkscopes.Where(y=>y.IsActive==true && y.IsDeleted==false)).ThenInclude(x=>x.Workscope)
                 .Include(x => x.InquiryWorkscopes.Where(y => y.IsActive == true && y.IsDeleted == false)).ThenInclude(x => x.Designs.Where(y => y.IsActive == true && y.IsDeleted == false)).ThenInclude(x => x.Files.Where(y => y.IsActive == true && y.IsDeleted == false))
                 .Include(x => x.InquiryWorkscopes.Where(y => y.IsActive == true && y.IsDeleted == false)).ThenInclude(x => x.Measurements.Where(y => y.IsActive == true && y.IsDeleted == false)).ThenInclude(x => x.Files.Where(y => y.IsActive == true && y.IsDeleted == false)).FirstOrDefault();
             if (inquiry == null)
