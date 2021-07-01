8a915d2cd332281b245b4e4db779f84321fb9c03
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-07-01 10:54:05 +0400

    Promo Controller added

---
+++ BackendSaiKitchen/Controllers/PromoController.cs
@@ -0,0 +1,113 @@
+﻿using BackendSaiKitchen.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Linq;
+
+namespace SaiKitchenBackend.Controllers
+{
+    public class PromoController : BaseController
+    {
+
+
+        [HttpPost]
+        [Route("[action]")]
+        public object GetAllPromo()
+        {
+            return promoRepository.FindByCondition(x => x.IsActive == true && x.IsDeleted == false);
+
+        }
+        [HttpGet]
+        [Route("[action]")]
+        public object GetPromoById(int promoId)
+        {
+            response.data = promoRepository.FindByCondition(x => x.PromoId == promoId && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+            if (response.data == null)
+            {
+                response.isError = true;
+                response.errorMessage = "Promo doesn't Exist";
+            }
+            return response;
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public object AddPromo(Promo promo)
+        {
+            if (promo.PromoId==0)
+            {
+                promoRepository.Create(promo);
+                context.SaveChanges();
+            }
+            else
+            {
+                Promo oldPromo = promoRepository.FindByCondition(x => x.PromoId == promo.PromoId && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+                if (oldPromo != null)
+                {
+                    oldPromo.PromoName = promo.PromoName;
+                    oldPromo.PromoStartDate = promo.PromoStartDate;
+                    oldPromo.PromoCode = promo.PromoCode;
+                    oldPromo.PromoCurrency = promo.PromoCurrency;
+                    oldPromo.PromoDescription = promo.PromoDescription;
+                    oldPromo.PromoDiscount = promo.PromoDiscount;
+                    oldPromo.PromoExpiryDate = promo.PromoExpiryDate;
+                    oldPromo.PromoTermsAndCondition = promo.PromoTermsAndCondition;
+                    oldPromo.IsPercentage = promo.IsPercentage;
+                    promoRepository.Update(oldPromo);
+                    context.SaveChanges();
+                    response.data = oldPromo;
+                }
+                else
+                {
+                    response.isError = true;
+                    response.errorMessage = "Promo doesn't exist";
+                }
+            }
+            return response;
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public object EditPromo(Promo promo)
+        {
+            Promo oldPromo = promoRepository.FindByCondition(x => x.PromoId == promo.PromoId && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+            if (oldPromo != null)
+            {
+                oldPromo.PromoName = promo.PromoName;
+                oldPromo.PromoStartDate = promo.PromoStartDate;
+                oldPromo.PromoCode = promo.PromoCode;
+                oldPromo.PromoCurrency = promo.PromoCurrency;
+                oldPromo.PromoDescription = promo.PromoDescription;
+                oldPromo.PromoDiscount = promo.PromoDiscount;
+                oldPromo.PromoExpiryDate = promo.PromoExpiryDate;
+                oldPromo.PromoTermsAndCondition = promo.PromoTermsAndCondition;
+                oldPromo.IsPercentage = promo.IsPercentage;
+                promoRepository.Update(oldPromo);
+                context.SaveChanges();
+                response.data = oldPromo;
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "Promo doesnt exist";
+            }
+            return response;
+        }
+        [HttpPost]
+        [Route("[action]")]
+        public object DeletePromo(int promoId)
+        {
+            Promo oldPromo = promoRepository.FindByCondition(x => x.PromoId == promoId && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+            if (oldPromo != null)
+            {
+                promoRepository.Delete(oldPromo);
+                context.SaveChanges();
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "Promo doesn't exist";
+            }
+            return response;
+        }
+
+    }
+}
