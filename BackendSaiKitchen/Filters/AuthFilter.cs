8da544aa8e447093cb550460f3c0331ef1c5c8c4
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-07-06 10:35:21 +0400

    Authorization Enabled

--- BackendSaiKitchen/Filters/AuthFilter.cs
+++ BackendSaiKitchen/Filters/AuthFilter.cs
@@ -22,7 +22,12 @@ namespace BackendSaiKitchen.ActionFilters
         public AuthFilter(int permission,int level)
         {
             this.permission = permission;
-            this.level = level;
+            this.level = level; 
+            branchRoleRepository = new Repository<BranchRole>(db);
+        }
+        public AuthFilter()
+        {
+            branchRoleRepository = new Repository<BranchRole>(db);
         }
 
         //public string resource { get; set; }
@@ -35,37 +40,38 @@ namespace BackendSaiKitchen.ActionFilters
 
         }
 
+        public BackendSaiKitchen_dbContext db = new BackendSaiKitchen_dbContext();
+        Repository<BranchRole> branchRoleRepository;
         public override void OnActionExecuting(ActionExecutingContext context)
         {
-            //    var v = context.HttpContext.Request.Method;
-            //    BackendSaiKitchen_dbContext db = new BackendSaiKitchen_dbContext();
-            //    Repository<BranchRole> branchRoleRepository = new Repository<BranchRole>(db);
-            //    StringValues branchRoleId;
-            //    context.HttpContext.Request.Headers.TryGetValue("BranchRoleId", out branchRoleId);
-            //    if (branchRoleId.Count > 0)
-            //        int.TryParse(branchRoleId[0], out Constants.branchRoleId);
-            //    try
-            //    {
-            //        var userPermissions = branchRoleRepository.FindByCondition(x => x.BranchRoleId == Constants.branchRoleId && x.IsActive == true && x.IsDeleted == false)
-            //                            .Include(y => y.PermissionRoles.Where(x => x.IsActive == true && x.IsDeleted == false))
-            //                            .ThenInclude(x => x.Permission)
-            //                            .Include(y => y.PermissionRoles.Where(x => x.IsActive == true && x.IsDeleted == false))
-            //                            .ThenInclude(x => x.PermissionLevel).FirstOrDefault();
-            //        var userperlevel = userPermissions?.PermissionRoles.Where(x => x.PermissionId == permission)?.FirstOrDefault()?.PermissionLevelId;
+            var v = context.HttpContext.Request.Method;
+            StringValues branchRoleId;
+            context.HttpContext.Request.Headers.TryGetValue("BranchRoleId", out branchRoleId);
+            if (branchRoleId.Count > 0)
+                int.TryParse(branchRoleId[0], out Constants.branchRoleId);
+            try
+            {
+                var userPermissions = branchRoleRepository.FindByCondition(x => x.BranchRoleId == Constants.branchRoleId && x.IsActive == true && x.IsDeleted == false)
+                                    .Include(y => y.PermissionRoles.Where(x => x.IsActive == true && x.IsDeleted == false))
+                                    .ThenInclude(x => x.Permission)
+                                    .Include(y => y.PermissionRoles.Where(x => x.IsActive == true && x.IsDeleted == false))
+                                    .ThenInclude(x => x.PermissionLevel).FirstOrDefault();
+                var userperlevel = userPermissions?.PermissionRoles.Where(x => x.PermissionId == permission)?.FirstOrDefault()?.PermissionLevelId;
 
-            //        if (userperlevel == null ||userperlevel - level < 0) {
+                if (userperlevel == null || userperlevel - level < 0)
+                {
 
-            //            response.isError = true;
-            //            response.errorMessage = Constants.UnAuthorizedUser;
-            //            context.Result = new OkObjectResult(response);
-            //        }
-            //    }
-            //    catch (Exception)
-            //    {
-            //        response.isError = true;
-            //        response.errorMessage = Constants.UnAuthorizedUser;
-            //        context.Result = new OkObjectResult(response);
-            //    }
+                    response.isError = true;
+                    response.errorMessage = Constants.UnAuthorizedUser;
+                    context.Result = new OkObjectResult(response);
+                }
+            }
+            catch (Exception)
+            {
+                response.isError = true;
+                response.errorMessage = Constants.UnAuthorizedUser;
+                context.Result = new OkObjectResult(response);
+            }
         }
     }
 }
