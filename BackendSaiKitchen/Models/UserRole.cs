a77dfb7ad77bea722126be16144ebab993f22a42
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-07-18 15:58:05 +0400

    User Role Model Updated

--- BackendSaiKitchen/Models/UserRole.cs
+++ BackendSaiKitchen/Models/UserRole.cs
@@ -18,7 +18,7 @@ namespace BackendSaiKitchen.Models
         public int? BranchRoleId { get; set; }
         public int? CreatedBy { get; set; }
         public string CreatedDate { get; set; }
-        public int? UpdaredBy { get; set; }
+        public int? UpdatedBy { get; set; }
         public string UpdatedDate { get; set; }
         public bool? IsActive { get; set; }
         public bool? IsDeleted { get; set; }
