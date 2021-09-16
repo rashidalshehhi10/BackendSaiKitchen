17f27d4cf77051ac51937130c6f59717b9d919a2
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-09-16 12:59:05 +0400

    Email Enabled

--- BackendSaiKitchen/Controllers/CustomerController.cs
+++ BackendSaiKitchen/Controllers/CustomerController.cs
@@ -181,25 +181,25 @@ namespace SaiKitchenBackend.Controllers
             {
                 customerRepository.Create(new Customer() { CustomerName = customer.CustomerName, CustomerContact = customer.CustomerContact, CustomerNotes = customer.CustomerNotes, CustomerEmail = customer.CustomerEmail,WayofContactId=customer.WayofContactId,ContactStatusId=customer.ContactStatusId });
 
-                //if (customer.CustomerEmail != null)
-                //{
-                //    //|| x.BranchRole.RoleTypeId == (int)roleType.Manager
-                //    var emails = userRoleRepository.FindByCondition(x => (x.BranchRole.RoleTypeId == (int)roleType.Sales) && x.IsActive == true && x.IsDeleted == false && x.User.IsActive == true && x.IsDeleted == false && x.BranchRole.IsActive == true && x.BranchRole.IsDeleted == false && x.Branch.IsActive == true && x.Branch.IsDeleted == false).Select(x => x.User.UserEmail).ToList();
-                //    foreach (var email in emails)
-                //    {
-                //        try
-                //        {
-                //            await mailService.SendEmailAsync(new MailRequest
-                //            {
-                //                Body = customer.CustomerNotes + "          \nName: " + customer.CustomerName + "\nContact: " + customer.CustomerContact + "\nEmail: " + customer.CustomerEmail,
-                //                Subject = "Request for Inquiry from New Customer " + customer.CustomerName,
-                //                ToEmail = email
-                //            });
-                //        }
+                if (customer.CustomerEmail != null)
+                {
+                    //|| x.BranchRole.RoleTypeId == (int)roleType.Manager
+                    var emails = userRoleRepository.FindByCondition(x => (x.BranchRole.RoleTypeId == (int)roleType.Sales) && x.IsActive == true && x.IsDeleted == false && x.User.IsActive == true && x.IsDeleted == false && x.BranchRole.IsActive == true && x.BranchRole.IsDeleted == false && x.Branch.IsActive == true && x.Branch.IsDeleted == false).Select(x => x.User.UserEmail).ToList();
+                    foreach (var email in emails)
+                    {
+                        try
+                        {
+                            await mailService.SendEmailAsync(new MailRequest
+                            {
+                                Body = customer.CustomerNotes + "          \nName: " + customer.CustomerName + "\nContact: " + customer.CustomerContact + "\nEmail: " + customer.CustomerEmail,
+                                Subject = "Request for Inquiry from New Customer " + customer.CustomerName,
+                                ToEmail = email
+                            });
+                        }
 
-                //        catch (Exception) { }
-                //    }
-                //}
+                        catch (Exception) { }
+                    }
+                }
             }
             else
             {
@@ -209,25 +209,25 @@ namespace SaiKitchenBackend.Controllers
                 {
                     oldCustomer.CustomerEmail = customer.CustomerEmail;
                 }
-                  //if (oldCustomer.CustomerEmail != null)
-                  //  {
-                  //      //|| x.BranchRole.RoleTypeId == (int)roleType.Manager
-                  //      var emails = userRoleRepository.FindByCondition(x => (x.BranchRole.RoleTypeId == (int)roleType.Sales) && x.IsActive == true && x.IsDeleted == false && x.User.IsActive == true && x.IsDeleted == false && x.BranchRole.IsActive == true && x.BranchRole.IsDeleted == false && x.Branch.IsActive == true && x.Branch.IsDeleted == false).Select(x => x.User.UserEmail).ToList();
-                  //      foreach (var email in emails)s
-                  //      {
-                  //      try
-                  //      {
+                if (oldCustomer.CustomerEmail != null)
+                {
+                    //|| x.BranchRole.RoleTypeId == (int)roleType.Manager
+                    var emails = userRoleRepository.FindByCondition(x => (x.BranchRole.RoleTypeId == (int)roleType.Sales) && x.IsActive == true && x.IsDeleted == false && x.User.IsActive == true && x.IsDeleted == false && x.BranchRole.IsActive == true && x.BranchRole.IsDeleted == false && x.Branch.IsActive == true && x.Branch.IsDeleted == false).Select(x => x.User.UserEmail).ToList();
+                    foreach (var email in emails) 
+                        {
+                        try
+                        {
 
-                  //          await mailService.SendEmailAsync(new MailRequest
-                  //          {
-                  //              Body = customer.CustomerNotes + "          \nName: " + customer.CustomerName + "\nContact: " + customer.CustomerContact + "\nEmail: " + oldCustomer.CustomerEmail,
-                  //              Subject = "Request for Inquiry from Customer " + customer.CustomerName,
-                  //              ToEmail = email
-                  //          });
-                  //      }
-                  //      catch (Exception) { }
-                  //  }
-                  //  }
+                            await mailService.SendEmailAsync(new MailRequest
+                            {
+                                Body = customer.CustomerNotes + "          \nName: " + customer.CustomerName + "\nContact: " + customer.CustomerContact + "\nEmail: " + oldCustomer.CustomerEmail,
+                                Subject = "Request for Inquiry from Customer " + customer.CustomerName,
+                                ToEmail = email
+                            });
+                        }
+                        catch (Exception) { }
+                    }
+                }
                 customerRepository.Update(oldCustomer);
             }
             context.SaveChanges();
