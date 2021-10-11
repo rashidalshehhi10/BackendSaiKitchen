0d09ce06df91a85311491d84dfeecfe8cf8c43f1
Sameer71095 <51756695+Sameer71095@users.noreply.github.com>
2021-10-11 12:11:40 +0400

    Quotation Controller Fixed

--- BackendSaiKitchen/Controllers/QuotationController.cs
+++ BackendSaiKitchen/Controllers/QuotationController.cs
@@ -175,7 +175,7 @@ namespace BackendSaiKitchen.Controllers
                       InquiryStartDate = Helper.Helper.GetDateFromString(x.InquiryStartDate),
                       //WorkScopeName = x.InquiryWorkscopes.Select(y => y.Workscope.WorkScopeName).First(),
                       //WorkScopeCount = x.InquiryWorkscopes.Count,
-                      Status = x.InquiryWorkscopes.FirstOrDefault().InquiryStatusId,
+                      Status = x.InquiryStatusId,
                       BuildingAddress = x.Building.BuildingAddress,
                       BuildingCondition = x.Building.BuildingCondition,
                       BuildingFloor = x.Building.BuildingFloor,
