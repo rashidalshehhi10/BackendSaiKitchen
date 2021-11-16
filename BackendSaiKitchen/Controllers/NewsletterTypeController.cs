d8f714aa7ba4fe209f8cfab5f18e1f00945cae2d
Ali Burhan <ali_burhan1993@outlook.com>
2021-11-16 11:57:43 +0200

    DeleteNewsletterType / UpdateNewsletterType / CreateNewsletterType

--- BackendSaiKitchen/Controllers/NewsletterTypeController.cs
+++ BackendSaiKitchen/Controllers/NewsletterTypeController.cs
@@ -1,4 +1,6 @@
-﻿using BackendSaiKitchen.Models;
+﻿using BackendSaiKitchen.CustomModel;
+using BackendSaiKitchen.Helper;
+using BackendSaiKitchen.Models;
 using Microsoft.AspNetCore.Mvc;
 using SaiKitchenBackend.Controllers;
 using System;
@@ -9,7 +11,7 @@ using System.Threading.Tasks;
 namespace BackendSaiKitchen.Controllers
 {
 
-    public class NewsletterType : BaseController
+    public class NewsletterTypeController : BaseController
     {
         [HttpPost]
         [Route("[action]")]
@@ -43,6 +45,78 @@ namespace BackendSaiKitchen.Controllers
         {
             var Frequency = newsletterFrequencyRepository.FindByCondition(x => x.IsActive == true && x.IsDeleted == false);
             response.data = Frequency;
+            return response;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public object CreateNewsletterType(NewsletterCustom type)
+        {
+            NewsletterType _type = new NewsletterType();
+            if (type != null)
+            {
+                _type.NewsletterTypeName = type.NewsletterTypeName;
+                _type.NewsletterTypeDescription = type.NewsletterTypeDescription;
+                _type.IsActive = true;
+                _type.IsDeleted = false;
+                _type.CreatedBy = Constants.userId;
+                _type.CreatedDate = Helper.Helper.GetDateTime();
+                newsletterTypeRepository.Create(_type);
+                response.data = _type;
+                context.SaveChanges();
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "Please ente the data correctly";
+            }
+            return response;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public object UpdateNewsletterType(NewsletterCustom type)
+        {
+            NewsletterType _type = newsletterTypeRepository.FindByCondition(x => x.NewsletterTypeId == type.NewsletterTypeId && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+            if (_type != null)
+            {
+                _type.NewsletterTypeName = type.NewsletterTypeName;
+                _type.NewsletterTypeDescription = type.NewsletterTypeDescription;
+                _type.IsActive = true;
+                _type.IsDeleted = false;
+                _type.UpdatedBy = Constants.userId;
+                _type.UpdatedDate = Helper.Helper.GetDateTime();
+                newsletterTypeRepository.Update(_type);
+                response.data = _type;
+                context.SaveChanges();
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "News letter Type Not Found";
+            }
+            return response;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public object DeleteNewsletterType(int NewsletterTypeId)
+        {
+            NewsletterType _type = newsletterTypeRepository.FindByCondition(x => x.NewsletterTypeId == NewsletterTypeId && x.IsActive == true && x.IsDeleted == false).FirstOrDefault();
+            if (_type != null)
+            {
+                _type.IsActive = false;
+                _type.UpdatedBy = Constants.userId;
+                _type.UpdatedDate = Helper.Helper.GetDateTime();
+                newsletterTypeRepository.Update(_type);
+                response.data = _type;
+                context.SaveChanges();
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "News letter Type Not Found";
+            }
             return response;
         }
     }
---
+++ BackendSaiKitchen/CustomModel/NewsletterCustom.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendSaiKitchen.CustomModel
+{
+    public class NewsletterCustom
+    {
+        public int NewsletterTypeId { get; set; }
+        public string NewsletterTypeDescription { get; set; }
+        public string NewsletterTypeName { get; set; }
+
+    }
+}
