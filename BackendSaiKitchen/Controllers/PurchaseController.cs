02a1c71c21b0fdf38046477f83a2f82841e822cc
Ali Burhan <ali_burhan1993@outlook.com>
2022-01-10 14:19:58 +0200

    GetInquirypurchaseRequestByBranchId / AddPurchaseOrder added

--- BackendSaiKitchen/Controllers/PurchaseController.cs
+++ BackendSaiKitchen/Controllers/PurchaseController.cs
@@ -13,6 +13,63 @@ namespace BackendSaiKitchen.Controllers
 {
     public class PurchaseController : BaseController
     {
+
+        [HttpPost]
+        [Route("[action]")]
+        public object GetInquirypurchaseRequestByBranchId(int branchId)
+        {
+            System.Collections.Generic.List<CheckListByBranch> inquiries = inquiryRepository.FindByCondition(x => x.BranchId == branchId && x.IsActive == true && x.IsDeleted == false
+                    && (x.InquiryStatusId == (int)inquiryStatus.jobOrderInProgress) && x.JobOrders.Any(y =>
+                        y.IsActive == true && y.IsDeleted == false && y.PurchaseRequests.Any(z => z.IsActive == true && z.IsDeleted == false)))
+                .Select(x => new CheckListByBranch
+                {
+                    InquiryId = x.InquiryId,
+                    QuotationNo = "QTN" + x.BranchId + "" + x.CustomerId + "" + x.InquiryId + "" + x.Quotations
+                        .OrderBy(y => y.QuotationId).LastOrDefault(y => y.IsActive == true && y.IsDeleted == false)
+                        .QuotationId,
+                    InquiryDescription = x.InquiryDescription,
+                    InquiryStartDate = Helper.Helper.GetDateFromString(x.InquiryStartDate),
+                    WorkScopeName = x.InquiryWorkscopes.Select(y => y.Workscope.WorkScopeName).ToList(),
+                    WorkScopeCount = x.InquiryWorkscopes.Count,
+                    Status = x.InquiryStatusId,
+                    BuildingAddress = x.Building.BuildingAddress,
+                    BuildingCondition = x.Building.BuildingCondition,
+                    BuildingFloor = x.Building.BuildingFloor,
+                    BuildingReconstruction = (bool)x.Building.BuildingReconstruction ? "Yes" : "No",
+                    IsOccupied = (bool)x.Building.IsOccupied ? "Yes" : "No",
+                    InquiryEndDate = Helper.Helper.GetDateFromString(x.InquiryEndDate),
+                    BuildingTypeOfUnit = x.Building.BuildingTypeOfUnit,
+                    IsEscalationRequested = x.IsEscalationRequested,
+                    CustomerId = x.CustomerId,
+                    CustomerCode = "CS" + x.BranchId + "" + x.CustomerId,
+                    CustomerName = x.Customer.CustomerName,
+                    CustomerEmail = x.Customer.CustomerEmail,
+                    CustomerContact = x.Customer.CustomerContact,
+                    BranchId = x.BranchId,
+                    InquiryAddedBy = x.ManagedByNavigation.UserName,
+                    InquiryAddedById = x.ManagedBy,
+                    NoOfRevision = x.Quotations.Where(y => y.IsDeleted == false).Count(),
+                    InquiryCode = "IN" + x.BranchId + "" + x.CustomerId + "" + x.InquiryId,
+                    CommentAddedOn = x.InquiryCommentsAddedOn,
+                    DesignAddedOn = x.InquiryWorkscopes.Where(x => x.IsActive == true && x.IsDeleted == false)
+                        .Select(x => x.DesignAddedOn).FirstOrDefault(),
+                    MeasurementAddedOn = x.InquiryWorkscopes.Where(x => x.IsActive == true && x.IsDeleted == false)
+                        .Select(x => x.MeasurementAddedOn).FirstOrDefault(),
+                    QuotationAddedOn = x.QuotationAddedOn
+                }).ToList();
+            if (inquiries != null)
+            {
+                response.data = inquiries;
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "there is no inquiries to Check";
+            }
+
+            return response;
+        }
+
         [HttpPost]
         [Route("[action]")]
         public object AddPurchaseRequest(PurchaseCustomModel purchase)
@@ -67,6 +124,66 @@ namespace BackendSaiKitchen.Controllers
                 response.isError = true;
                 response.errorMessage = "inquiry Not Found";
             }
+            return response;
+        }
+
+        [HttpPost]
+        [Route("[action]")]
+        public object AddPurchaseOrder(PurchaseOrderCustomModel purchase)
+        {
+            var inquiry = inquiryRepository.FindByCondition(x => x.IsActive == true && x.IsDeleted == false && x.InquiryStatusId == (int)inquiryStatus.jobOrderInProgress)
+                .Include(x => x.JobOrders.Where(x => x.IsActive == true && x.IsDeleted == false))
+                .ThenInclude(x => x.PurchaseRequests.Where(x => x.IsActive == true && x.IsDeleted == false))
+                .ThenInclude(x => x.PurchaseOrders.Where(x => x.IsActive == true && x.IsDeleted == false)).FirstOrDefault();
+            if (inquiry != null)
+            {
+                foreach (var job in inquiry.JobOrders)
+                
+                {
+                    foreach (var purreq in job.PurchaseRequests)
+                    {
+                        foreach (var order in purchase.lpo)
+                        {
+                            PurchaseOrder Order = new PurchaseOrder();
+                            Order.PurchaseOrderActualDeliveryDate = order.PurchaseOrderActualDeliveryDate;
+                            Order.PurchaseOrderAmount = order.PurchaseOrderAmount;
+                            Order.IsActive = true;
+                            Order.IsDeleted = false;
+                            Order.CreatedBy = Constants.userId;
+                            Order.CreatedDate = Helper.Helper.GetDateTime();
+                            foreach (string fileUrl in order.files)
+                            {
+                                if (fileUrl != null && fileUrl != string.Empty)
+                                {
+                                    Order.Files.Add(new File
+                                    {
+                                        FileUrl = fileUrl,
+                                        FileName = fileUrl.Split('.')[0],
+                                        FileContentType = fileUrl.Split('.').Length > 1 ? fileUrl.Split('.')[1] : "mp4",
+                                        IsImage = fileUrl.Split('.').Length > 1,
+                                        IsActive = true,
+                                        IsDeleted = false,
+                                        UpdatedBy = Constants.userId,
+                                        UpdatedDate = Helper.Helper.GetDateTime(),
+                                        CreatedBy = Constants.userId,
+                                        CreatedDate = Helper.Helper.GetDateTime()
+                                    });
+                                }
+                            }
+                            purreq.PurchaseOrders.Add(Order);
+                        }
+                    }
+                }
+                inquiryRepository.Update(inquiry);
+                response.data = inquiry;
+                context.SaveChanges();
+            }
+            else
+            {
+                response.isError = true;
+                response.errorMessage = "inquiry Not Found";
+            }
+
             return response;
         }
     }
---
+++ BackendSaiKitchen/CustomModel/PurchaseOrderCustomModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BackendSaiKitchen.CustomModel
+{
+    public class PurchaseOrderCustomModel
+    {
+        public int inquiryId { get; set; }
+        public List<Order> lpo { get; set; }
+    }
+
+    public class Order
+    {
+        public string PurchaseOrderAmount { get; set; }
+        public string PurchaseOrderActualDeliveryDate { get; set; }
+        public List<string> files { get; set; }
+    }
+}
